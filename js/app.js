1e34b2399c5f3617daaafda27dd93eb1ccf61e19
Evan Smith <smith.m.evan@gmail.com>
2020-01-10 16:33:10 +0000

    film listeners seem to be working and grabbing the data, resetting the form

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+
   console.log('JS has loaded')
 
+  const newFormElement = document.querySelector('#whole-form');
+  newFormElement.addEventListener('submit', handleNewFormElementSubmit);
 
-  
 })
+
+const handleNewFormElementSubmit = function (event) {
+  event.preventDefault();
+
+  const filmListItem = createFilmListItem(event.target);
+  const filmList = document.querySelector('#film-list');
+  filmList.appendChild(filmListItem);
+
+  event.target.reset();
+}
